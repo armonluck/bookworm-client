b50eff3740e2b828caeeae48fc682d3db7611176
Armon Luckenbach <armonluck@gmail.com>
2023-08-25 00:34:58 -0400

    Adding component files to help plan client structure and adding barebones login placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
 import './App.scss';
 
 function App() {
   return (
     <BrowserRouter>
+      <div>
+        <h1>bookworm</h1>
+      </div>
+
       <Routes>
         <Route path='/' element='' />
         <Route path='/home' element='' />
         <Route path='/about' element='' />
         <Route path='/contact' element='' />
-        <Route path='/login' element='' />
+        <Route path='/login' element={ <Login />} />
         <Route path='/profile' element='' />
         <Route path='/messages' element='' />
         <Route path='/browse' element='' />
       </Routes>
-
-      <div>
-        <h1>bookworm</h1>
-      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import './Login.scss';
+
+function Login () {
+    return (
+        <div>
+            <h1>Login / Sign Up</h1>
+            <p>This is where you login and make a new account</p>
+        </div>
+    )
+}
+
+export default Login;
