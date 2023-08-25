b4024ce94baa052e737a5e144bb47e395baa925d
Armon Luckenbach <armonluck@gmail.com>
2023-08-24 23:50:45 -0400

    Created multiple page files to help plan site layout and added routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 function App() {
   return (
-    <div>
-      <h1>bookworm</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element='' />
+        <Route path='/home' element='' />
+        <Route path='/about' element='' />
+        <Route path='/contact' element='' />
+        <Route path='/login' element='' />
+        <Route path='/profile' element='' />
+        <Route path='/messages' element='' />
+        <Route path='/browse' element='' />
+      </Routes>
+
+      <div>
+        <h1>bookworm</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
