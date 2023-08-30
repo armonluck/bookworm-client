8094e0e6760a013a3bf253b06e6a31516dd14505
Armon Luckenbach <armonluck@gmail.com>
2023-08-29 21:41:45 -0400

    Created async function to send new messages and added useState to track message data

--- src/pages/MessagesPage/MessagesPage.jsx
+++ src/pages/MessagesPage/MessagesPage.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import images from '../../components/Images/Images';
 import './MessagesPage.scss';
 
-function MessagesPage ({socket, username, room}) {
+function MessagesPage({ socket, username, room }) {
+
+    const [currentMessage, setCurrentMessage] = useState("");
+
+    const sendMessage = async () => {
+        if (currentMessage !== "") {
+            const messageData = {
+                room: room,
+                author: username,
+                message: currentMessage,
+                time: new Date(Date.now()).toLocaleTimeString()
+            };
+
+            await socket.emit("send_message", messageData);
+        }
+    };
 
     return (
         <div>
@@ -13,13 +28,19 @@ function MessagesPage ({socket, username, room}) {
 
             </div>
             <div className='footer'>
-                <input type="text" placeholder="Message..." name="" id="" />
-                <button>&#9658;</button>
-                <button>
-                    <img src={images.Send} alt="" />
+                <input
+                    type="text"
+                    placeholder="Message..."
+                    name="message"
+                    onChange={(event) => {
+                        setCurrentMessage(event.target.value);
+                    }} />
+                <button onClick={sendMessage}>
+                    Send
+                    <img src={images.Send} alt="Send SVG" />
                 </button>
             </div>
-            
+
         </div>
     )
 }
