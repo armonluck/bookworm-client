e45a76a4d31fbb1366170237ac56213c3a7bef55
Armon Luckenbach <armonluck@gmail.com>
2023-09-09 09:33:10 -0400

    Successfully moved socket.io code to separate page and component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import { io } from 'socket.io-client';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -11,56 +9,20 @@ import AboutPage from './pages/AboutPage/AboutPage';
 import HomePage from './pages/HomePage/HomePage';
 import './App.scss';
 
-// socket variable shows url for the backend server
-const socket = io.connect('http://localhost:7070');
-
 function App() {
-
-  // State variables to define username and room inputs
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-
-  // Fx connects users who join the same room for a chat
-  const handleJoinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room)
-    }
-  };
 
   return (
     <BrowserRouter>
       <Header />
-
-      <div className='chat-form'>
-        <h3 className='chat-form__subtitle'>Name</h3>
-        <input
-          className='chat-form__input'
-          type="text"
-          placeholder="Add Your Name..."
-          name="username"
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }} />
-        <h3 className='chat-form__subtitle'>Room</h3>
-        <input
-          className='chat-form__input'
-          type="text"
-          placeholder="Room ID..."
-          name="room"
-          onChange={(event) => {
-            setRoom(event.target.value);
-          }} />
-        <br />
-        <button
-          className='chat-form__btn'
-          onClick={handleJoinRoom}>Join A Room</button>
+      <div>
+        <h1>hi</h1>
       </div>
 
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/home' element={<HomePage />} />
         <Route path='/browse' element={<BrowsePage />} />
-        <Route path='/messages' element={<MessagesPage socket={socket} username={username} room={room} />} />
+        <Route path='/messages' element={<MessagesPage />} />
         <Route path='/about' element={<AboutPage />} />
         <Route path='/contact' element='' />
         <Route path='/login' element={<Login />} />
---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import images from '../Images/Images';
+import './Messages.scss';
+
+function Messages({ socket, username, room }) {
+
+  // State variables to define current message info and the message list or array
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageArray, setMessageArray] = useState([]);
+
+  // Async function to handle sending a message
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        username: username,
+        message: currentMessage,
+        time: new Date(Date.now()).toLocaleTimeString()
+      };
+
+      // Sends messageData to websocket, updates the array, and clears out the input values
+      await socket.emit("send_message", messageData);
+      setMessageArray((prevArray) => [...prevArray, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageArray((prevArray) => [...prevArray, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className='messages-container'>
+      <div className='messages-header'>
+        <p className='messages-header__title'>Book Discussion In Progress</p>
+      </div>
+
+      <div className='messages-main'>
+        {messageArray?.map((messageContent) => (
+          //ternary operator to change id for styling
+          <div className='messages-main__container' id={username === messageContent.username ? "self" : "other"}>
+            <div className='messages-main__content'>
+              <p>{messageContent.message}</p>
+            </div>
+            <div className='messages-main__meta'>
+              <p>{messageContent.username}</p>
+              <p>{messageContent.time}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='messages-footer'>
+        <input
+          className='messages-footer__input'
+          type='text'
+          value={currentMessage}
+          placeholder="Message..."
+          name="message"
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          // Call fx using "Enter" key or Btn click below
+          onKeyDown={(event) => {
+            event.key === "Enter" && sendMessage();
+          }} />
+        <button className='messages-footer__btn' onClick={sendMessage}>
+          Send
+          <img className='messages-footer__svg' src={images.Send} alt="Send icon SVG" />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Messages;
