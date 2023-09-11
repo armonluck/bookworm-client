75063ed256a49291e388f20278a7818c22be2394
Armon Luckenbach <armonluck@gmail.com>
2023-09-10 22:56:52 -0400

    Removed large img file and replaced with smaller options, getting bug that prevents upload to remote github

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -3,11 +3,14 @@ import './AboutPage.scss'
 
 function AboutPage() {
   return (
-    <div className='about'>
-      <p className='about__bio'>
-        My name is Armon Luckenbach, the humble developer of the app before you. Websites and applications that appeal to the reading community have failed to bring a social connection between readers. Although comments, reviews, and occasional discussions under individual book titles are possible the user cannot have a private discussion/chat in app and have to navigate to another platform to properly express themselves. This is where bookworm steps in to bridge that gap and fulfill the need to jump into a heated discussion with friends after getting to the climax of a sci-fi novel.
-      </p>
-    </div>
+    <section className='about'>
+      <h2>Meet The Team</h2>
+      <p className='about__bio'>Armon Luckenbach</p>
+      <p>Lead Developer</p>
+      <p>The humble developer of the app before you. Websites and applications that appeal to the reading community have failed to bring a social connection between readers. Although comments, reviews, and occasional discussions under individual book titles are possible the user cannot have a private discussion/chat in app and have to navigate to another platform to properly express themselves. This is where bookworm steps in to bridge that gap and fulfill the need to jump into a heated discussion with friends after getting to the climax of a sci-fi novel.</p>
+      <h2></h2>
+      <p>Reading doesn’t have to be a solo endeavor. Popular apps in the reading community have failed to bring a social connection between readers which is where bookworm steps in to bridge that gap</p>
+    </section>
   )
 }
 
